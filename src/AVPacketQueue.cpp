39ad70607e8b8b888f5ed0595e4d05e56ca8cc94
zhouweimin <354412101@qq.com>
2019-07-30 16:09:44 +0800

    rebuild

--- src/AVPacketQueue.cpp
+++ src/AVPacketQueue.cpp
@@ -17,19 +17,15 @@ namespace simple_player {
     }
 
     void AVPacketQueue::push(AVPacket *pkt) {
-        std::lock_guard<std::mutex> lock(queue_mutex_);
-        while(queue_.size() >= max_queue_size_) {
-            not_full_.wait(queue_mutex_);
-        }
+        std::unique_lock<std::mutex> lock(queue_mutex_);
+        not_full_.wait(queue_mutex_, [this]{return queue_.size() < max_queue_size_;});
         queue_.push(pkt);
         not_empty_.notify_one();
     }
 
     AVPacket * AVPacketQueue::pop() {
-        std::lock_guard<std::mutex> lock(queue_mutex_);
-        while(queue_.empty()) {
-            not_empty_.wait(queue_mutex_);
-        }
+        std::unique_lock<std::mutex> lock(queue_mutex_);
+        not_empty_.wait(queue_mutex_, [this]{return !queue_.empty();});
         AVPacket *pkt = queue_.front();
         queue_.pop();
         not_full_.notify_one();
